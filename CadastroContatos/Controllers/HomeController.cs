068f0affba8913ae68a13e25ad755f5f90e24857
othongonzaga <othoncesar1@hotmail.com>
2022-09-11 12:56:07 -0300

    Começando projeto

---
+++ CadastroContatos/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using CadastroContatos.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroContatos.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            HomeModel home = new HomeModel();
+            home.Nome = "Othon";
+            home.Email = "othon@oi.com";
+
+            return View(home);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
